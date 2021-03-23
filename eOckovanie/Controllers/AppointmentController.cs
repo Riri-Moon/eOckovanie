38c9677b5dbac5da21f5e7bced7b9cd5483ce8bd
Riri-Moon <38843958+Riri-Moon@users.noreply.github.com>
2021-03-23 11:24:25 +0100

    Removed smthing

--- eOckovanie/Controllers/AppointmentController.cs
+++ eOckovanie/Controllers/AppointmentController.cs
@@ -1,11 +1,7 @@
 ﻿using eOckovanieData.Library.DataAccess;
 using eOckovanieData.Library.Models;
 using Microsoft.AspNet.Identity;
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Net;
-using System.Net.Http;
 using System.Web.Http;
 
 namespace eOckovanie.Controllers
